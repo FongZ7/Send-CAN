acb1df2c8080022fe9650e092b5e15bbc246c197
Whatcharawit <w.whatcharawit@gmail.com>
2025-03-19 00:45:46 +0700

    Convert int to Hex

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,10 @@ uint8_t txData[8] = {0x00, 0x01, 0x23, 0x45, 0xAB, 0xCD, 0xEF, 0xFF};
 const uint32_t txDly = 2000;  // ส่งข้อมูลทุก 2 วินาที
 uint32_t lastSendTime = 0;
 
+
+uint8_t values[8] = {12, 45, 78, 123, 200, 255, 5, 90}; 
+uint8_t hexValue[8];
+
 void setup() {
   mySerial.begin(115200);
   pinMode(LED_PIN, OUTPUT);
@@ -17,6 +21,24 @@ void setup() {
 
   // ตั้งค่าให้รับเฉพาะ ID ที่ต้องการ
   can.filterMask16Init(0, 0x070, 0x7FF, 0, 0);
+
+  for (int i = 0; i < 8; i++) {
+    hexValue[i] = (uint8_t)values[i]; // Convert and store
+  }
+  
+  // Print the stored hex values
+  mySerial.print("Hex Values: ");
+  for (int i = 0; i < 8; i++) {
+    mySerial.print("0x");
+    mySerial.print(hexValue[i], HEX);
+    mySerial.print(" ");
+  }
+  mySerial.print("tx Data: ");
+  for (int i = 0; i < 8; i++) {
+    mySerial.print("0x");
+    mySerial.print(txData[i], HEX);
+    mySerial.print(" ");
+  }
 }
 
 void loop() {
@@ -26,7 +48,7 @@ void loop() {
       mySerial.print("📤 Sending CAN ID: 0x");
       mySerial.println(txMsgID, HEX);
 
-      bool sent = can.transmit(txMsgID, txData, sizeof(txData));
+      bool sent = can.transmit(txMsgID, hexValue, sizeof(hexValue));
 
       if (sent) {
           mySerial.println("✅ CAN Message Sent!");
